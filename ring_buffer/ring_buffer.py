b2385084c177a22508468c708a73cfaa307784f1
Sameera Leola <sameeraleola@gmail.com>
2020-04-24 11:01:02 -0700

    Reverse complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,25 +8,65 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # # Oldest is first in (head)
+        # Make sure there's room
+        if len(self.storage) < self.capacity:
+            # It is not full so add it to the tail
+            self.storage.add_to_tail(item)
+            # Keep track of the last item added
+            self.current = self.storage.tail
+        else:
+            # Remove the head then add the new node
+            new_head = self.storage.head.next
+                self.next.prev = self.prev
+
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+               
+        #if not at capacity:
+        # add to the most recent
+        # update current
+        # if len(self.storage) < self.capacity:
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.tail
+        
+        # # if at capacity
+        # # discard stale value
+        # # checking index of other values
+        # # moving through each of them
+        # # if the current value is the most recent, start at the beginning
+        # if len(self.storage) == self.capacity:
+        #     self.current.value = item
+        #     if self.current is self.storage.tail:
+        #         self.current = self.storage.head
+        #     else:    
+        #          self.current = self.current.next
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
-
+        item = self.storage.head
+        while item:
+            if item.value i!= None:
+                list_buffer_contents.append(item.value)
+        
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
