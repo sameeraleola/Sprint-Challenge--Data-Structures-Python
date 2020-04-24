03cc3ae114430f26f52a46fb99cc10b2cd493c80
Sameera Leola <sameeraleola@gmail.com>
2020-04-24 08:57:01 -0700

    Completed names

--- names/names.py
+++ names/names.py
@@ -1,3 +1,9 @@
+# The nested
+# The obvious choice is to implement a binary search
+# The BST class and the compare method is required.
+# To save time I will import the one that is already written.
+# Wouold be nice to implement from scratch, but this is a timed Sprint Challenge!
+from binary_search_tree import BinarySearchTree
 import time
 
 start_time = time.time()
@@ -13,14 +19,35 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print ("Nested Loops")
+# print (f"runtime: {end_time - start_time} seconds")
+
+# Create an instance of the BST
+# Grrrr! Must give it the root value!
+bst = BinarySearchTree(names_1[0])  
+
+# Add the names in the name_1 file
+for n1 in names_1:
+    bst.insert(n1)
+
+# Now check each name in the name_2 file fir dups
+# If the name is a duplicate add it to the duplicate list
+for n2 in names_2:
+    if bst.contains(n2):
+        duplicates.append(n2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
